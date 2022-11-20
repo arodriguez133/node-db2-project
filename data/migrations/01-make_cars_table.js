a37540d5834e7876606eaae9f016983f17bd96eb
arodriguez133 <arodriguez133@icloud.com>
2022-11-20 16:44:05 -0600

    added table paramters using knex

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,5 +1,20 @@
+
+
 exports.up = function (knex) {
-  // DO YOUR MAGIC
+  return knex.schema.createTable('cars', (tbl) => {
+    tbl.increments();
+    tbl.string('vin')
+      .dropNullable()
+      .unique();
+    tbl.string('make')
+      .dropNullable();
+    tbl.string('model')
+      .dropNullable();
+    tbl.decimal('mileage', { precision: null })
+      .dropNullable();
+    tbl.string('title');
+    tbl.string('tansmission');
+  })
 };
 
 exports.down = function (knex) {
