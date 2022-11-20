857610838191c95f9d21f4336619c3c40bb4b5d7
arodriguez133 <arodriguez133@icloud.com>
2022-11-20 17:33:57 -0600

    added a 'get all' route using the new database. There are no cars yet so there will be nothing that the client wil recieve at this time. Now I have to build the server

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,23 @@
-// DO YOUR MAGIC
+const Car = require('./cars-model');
+const router = express().Router();
+
+
+router.get('/', async (req, res, next) => {
+    try {
+        const cars = await Car.getAll();
+        res.json(200).json(cars);
+    }
+    catch (err) {
+        next(err)
+    }
+})
+
+router.use('/', (err, req, res, next) => {
+    res.status(500).json({
+        message: "Could not process request at this time",
+        err: err.message
+    })
+})
+
+
+module.exports = router;
