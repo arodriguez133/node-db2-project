7d94619c7bef4f5abecc32b4998c5af860cff1cf
arodriguez133 <arodriguez133@icloud.com>
2022-11-20 21:09:42 -0600

    checkCarId middleware finished

--- api/cars/cars-middleware.js
+++ api/cars/cars-middleware.js
@@ -1,5 +1,18 @@
-const checkCarId = (req, res, next) => {
-  // DO YOUR MAGIC
+const Car = require('./cars-model');
+
+
+const checkCarId = async (req, res, next) => {
+  const id = req.params.id;
+  try {
+    const car = Car.getById(id);
+    if (!car) {
+      res.status(404).json({
+        message: `car with id ${id} is not found`
+      })
+    } else {
+      next();
+    }
+  }
 }
 
 const checkCarPayload = (req, res, next) => {
